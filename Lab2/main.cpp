e768f8912ce524af2acc5f299b7b95aeb3b62bef
Naxaes <ted.kleinman@hotmail.com>
2021-04-01 14:17:47 +0200

    Update main.cpp
    Fixed alignment

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -63,20 +63,20 @@ void Update(float dt)
 {
     std::cout << "Render time: " << dt << " s." << std::endl;
 
-	const Uint8* key_state = SDL_GetKeyboardState(nullptr);
+    const Uint8* key_state = SDL_GetKeyboardState(nullptr);
 
-	if (key_state[SDL_SCANCODE_UP])     { std::cout << "Pressing UP"	<< std::endl; }
-	if (key_state[SDL_SCANCODE_DOWN])   { std::cout << "Pressing DOWN"	<< std::endl; }
-	if (key_state[SDL_SCANCODE_LEFT])   { std::cout << "Pressing LEFT"	<< std::endl; }
-	if (key_state[SDL_SCANCODE_RIGHT])  { std::cout << "Pressing RIGHT"	<< std::endl; }
-	if (key_state[SDL_SCANCODE_RSHIFT]) { std::cout << "Pressing RSHIFT"    << std::endl; }
-	if (key_state[SDL_SCANCODE_RCTRL])  { std::cout << "Pressing RCTRL"	<< std::endl; }
-	if (key_state[SDL_SCANCODE_W])      { std::cout << "Pressing W"		<< std::endl; }
-	if (key_state[SDL_SCANCODE_S])      { std::cout << "Pressing S"		<< std::endl; }
-	if (key_state[SDL_SCANCODE_A])      { std::cout << "Pressing A"		<< std::endl; }
-	if (key_state[SDL_SCANCODE_D])      { std::cout << "Pressing D"		<< std::endl; }
-	if (key_state[SDL_SCANCODE_E])      { std::cout << "Pressing E"		<< std::endl; }
-	if (key_state[SDL_SCANCODE_Q])      { std::cout << "Pressing Q"		<< std::endl; }
+    if (key_state[SDL_SCANCODE_UP])     { std::cout << "Pressing UP"     << std::endl; }
+    if (key_state[SDL_SCANCODE_DOWN])   { std::cout << "Pressing DOWN"   << std::endl; }
+    if (key_state[SDL_SCANCODE_LEFT])   { std::cout << "Pressing LEFT"   << std::endl; }
+    if (key_state[SDL_SCANCODE_RIGHT])  { std::cout << "Pressing RIGHT"  << std::endl; }
+    if (key_state[SDL_SCANCODE_RSHIFT]) { std::cout << "Pressing RSHIFT" << std::endl; }
+    if (key_state[SDL_SCANCODE_RCTRL])  { std::cout << "Pressing RCTRL"  << std::endl; }
+    if (key_state[SDL_SCANCODE_W])      { std::cout << "Pressing W"      << std::endl; }
+    if (key_state[SDL_SCANCODE_S])      { std::cout << "Pressing S"      << std::endl; }
+    if (key_state[SDL_SCANCODE_A])      { std::cout << "Pressing A"      << std::endl; }
+    if (key_state[SDL_SCANCODE_D])      { std::cout << "Pressing D"      << std::endl; }
+    if (key_state[SDL_SCANCODE_E])      { std::cout << "Pressing E"      << std::endl; }
+    if (key_state[SDL_SCANCODE_Q])      { std::cout << "Pressing Q"      << std::endl; }
 }
 
 void Draw(Window& window)
